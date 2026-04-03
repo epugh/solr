a5c6ccca6bafdf52f70a0878323ea9fc6ced6764
Eric Pugh <epugh@opensourceconnections.com>
2026-04-03 17:28:21 -0400

    Add download and get file to configsets api.  Update docs.
    Separated out of the larger PR around SchemaDesigner.

---
+++ solr/core/src/java/org/apache/solr/handler/configsets/GetConfigSetFile.java
@@ -0,0 +1,82 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.solr.handler.configsets;
+
+import static org.apache.solr.security.PermissionNameProvider.Name.CONFIG_READ_PERM;
+
+import jakarta.inject.Inject;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.apache.solr.client.api.endpoint.ConfigsetsApi;
+import org.apache.solr.client.api.model.ConfigSetFileContentsResponse;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.util.StrUtils;
+import org.apache.solr.core.CoreContainer;
+import org.apache.solr.jersey.PermissionName;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.response.SolrQueryResponse;
+
+/** V2 API implementation for reading the contents of a single file from an existing configset. */
+public class GetConfigSetFile extends ConfigSetAPIBase implements ConfigsetsApi.GetFile {
+
+  @Inject
+  public GetConfigSetFile(
+      CoreContainer coreContainer,
+      SolrQueryRequest solrQueryRequest,
+      SolrQueryResponse solrQueryResponse) {
+    super(coreContainer, solrQueryRequest, solrQueryResponse);
+  }
+
+  @Override
+  @PermissionName(CONFIG_READ_PERM)
+  public ConfigSetFileContentsResponse getConfigSetFile(String configSetName, String filePath)
+      throws Exception {
+    if (StrUtils.isNullOrEmpty(configSetName)) {
+      throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, "No configset name provided");
+    }
+    if (StrUtils.isNullOrEmpty(filePath)) {
+      throw new SolrException(SolrException.ErrorCode.BAD_REQUEST, "No file path provided");
+    }
+    if (!configSetService.checkConfigExists(configSetName)) {
+      throw new SolrException(
+          SolrException.ErrorCode.NOT_FOUND, "ConfigSet '" + configSetName + "' not found");
+    }
+    byte[] data = downloadFileFromConfig(configSetName, filePath);
+    final var response = instantiateJerseyResponse(ConfigSetFileContentsResponse.class);
+    response.path = filePath;
+    response.content =
+        data != null && data.length > 0 ? new String(data, StandardCharsets.UTF_8) : "";
+    return response;
+  }
+
+  private byte[] downloadFileFromConfig(String configSetName, String filePath) {
+    try {
+      final byte[] data = configSetService.downloadFileFromConfig(configSetName, filePath);
+      if (data == null) {
+        throw new SolrException(
+            SolrException.ErrorCode.NOT_FOUND,
+            "File '" + filePath + "' not found in configset '" + configSetName + "'");
+      }
+      return data;
+    } catch (IOException e) {
+      throw new SolrException(
+          SolrException.ErrorCode.NOT_FOUND,
+          "File '" + filePath + "' not found in configset '" + configSetName + "'",
+          e);
+    }
+  }
+}
---
+++ solr/core/src/test/org/apache/solr/handler/configsets/GetConfigSetFileAPITest.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.solr.handler.configsets;
+
+import static org.apache.solr.SolrTestCaseJ4.assumeWorkingMockito;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.apache.solr.SolrTestCase;
+import org.apache.solr.client.api.model.ConfigSetFileContentsResponse;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.core.CoreContainer;
+import org.apache.solr.core.FileSystemConfigSetService;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/** Unit tests for {@link GetConfigSetFile}. */
+public class GetConfigSetFileAPITest extends SolrTestCase {
+
+  private CoreContainer mockCoreContainer;
+  private FileSystemConfigSetService configSetService;
+  private Path configSetBase;
+
+  @BeforeClass
+  public static void ensureWorkingMockito() {
+    assumeWorkingMockito();
+  }
+
+  @Before
+  public void initConfigSetService() {
+    configSetBase = createTempDir("configsets");
+    // Use an anonymous subclass to access the protected testing constructor
+    configSetService = new FileSystemConfigSetService(configSetBase) {};
+    mockCoreContainer = mock(CoreContainer.class);
+    when(mockCoreContainer.getConfigSetService()).thenReturn(configSetService);
+  }
+
+  /** Creates a configset directory with one file. */
+  private void createConfigSetWithFile(String configSetName, String filePath, String content)
+      throws Exception {
+    Path dir = configSetBase.resolve(configSetName);
+    Files.createDirectories(dir);
+    Files.writeString(dir.resolve(filePath), content, StandardCharsets.UTF_8);
+  }
+
+  @Test
+  public void testMissingConfigSetNameThrowsBadRequest() {
+    final var api = new GetConfigSetFile(mockCoreContainer, null, null);
+    final var ex =
+        assertThrows(SolrException.class, () -> api.getConfigSetFile(null, "schema.xml"));
+    assertEquals(SolrException.ErrorCode.BAD_REQUEST.code, ex.code());
+
+    final var ex2 = assertThrows(SolrException.class, () -> api.getConfigSetFile("", "schema.xml"));
+    assertEquals(SolrException.ErrorCode.BAD_REQUEST.code, ex2.code());
+  }
+
+  @Test
+  public void testMissingFilePathThrowsBadRequest() {
+    final var api = new GetConfigSetFile(mockCoreContainer, null, null);
+    final var ex = assertThrows(SolrException.class, () -> api.getConfigSetFile("myconfig", null));
+    assertEquals(SolrException.ErrorCode.BAD_REQUEST.code, ex.code());
+
+    final var ex2 = assertThrows(SolrException.class, () -> api.getConfigSetFile("myconfig", ""));
+    assertEquals(SolrException.ErrorCode.BAD_REQUEST.code, ex2.code());
+  }
+
+  @Test
+  public void testNonExistentConfigSetThrowsNotFound() {
+    // "missing" was never created in configSetBase, so checkConfigExists returns false
+    final var api = new GetConfigSetFile(mockCoreContainer, null, null);
+    final var ex =
+        assertThrows(SolrException.class, () -> api.getConfigSetFile("missing", "schema.xml"));
+    assertEquals(SolrException.ErrorCode.NOT_FOUND.code, ex.code());
+  }
+
+  @Test
+  public void testSuccessfulFileRead() throws Exception {
+    final String configSetName = "myconfig";
+    final String filePath = "schema.xml";
+    final String fileContent = "<schema/>";
+    createConfigSetWithFile(configSetName, filePath, fileContent);
+
+    final var api = new GetConfigSetFile(mockCoreContainer, null, null);
+    final ConfigSetFileContentsResponse response = api.getConfigSetFile(configSetName, filePath);
+
+    assertNotNull(response);
+    assertEquals(filePath, response.path);
+    assertEquals(fileContent, response.content);
+  }
+
+  @Test
+  public void testFileNotFoundInConfigSetThrowsNotFound() throws Exception {
+    final String configSetName = "myconfig";
+    // Create the configset directory but do NOT add the requested file
+    Files.createDirectories(configSetBase.resolve(configSetName));
+
+    final var api = new GetConfigSetFile(mockCoreContainer, null, null);
+    final var ex =
+        assertThrows(SolrException.class, () -> api.getConfigSetFile(configSetName, "missing.xml"));
+    assertEquals(SolrException.ErrorCode.NOT_FOUND.code, ex.code());
+  }
+
+  @Test
+  public void testEmptyFileReturnsEmptyContent() throws Exception {
+    final String configSetName = "myconfig";
+    final String filePath = "empty.xml";
+    createConfigSetWithFile(configSetName, filePath, "");
+
+    final var api = new GetConfigSetFile(mockCoreContainer, null, null);
+    final ConfigSetFileContentsResponse response = api.getConfigSetFile(configSetName, filePath);
+
+    assertNotNull(response);
+    assertEquals(filePath, response.path);
+    assertEquals("", response.content);
+  }
+}
